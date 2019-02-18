c59c97384eac0b844fd6fe0240b8d706a1c89b98
Néstor Salceda <nestor.salceda@sysdig.com>
2019-02-18 18:20:52 +0100

    Fix productpage python tests

--- samples/bookinfo/src/productpage/tests/unit/test_productpage.py
+++ samples/bookinfo/src/productpage/tests/unit/test_productpage.py
@@ -37,22 +37,12 @@ class ApplianceTest(unittest.TestCase):
         # Register expected headers with the mock. If the headers
         # don't match, the mock won't fire, an E500 will be triggered
         # and the test will fail.
-        expected_headers = {
-            'x-request-id': '34eeb41d-d267-9e49-8b84-dde403fc5b72',
-            'x-b3-traceid': '40c7fdf104e3de67',
-            'x-b3-spanid': '40c7fdf104e3de67',
-            'x-b3-sampled': '1'
-        }
+        expected_headers = {}
         m.get("http://reviews:9080/reviews/%d" % product_id, text='{}',
               request_headers=expected_headers)
 
         uri = "/api/v1/products/%d/reviews" % product_id
-        headers = {
-            'x-request-id': '34eeb41d-d267-9e49-8b84-dde403fc5b72',
-            'x-b3-traceid': '40c7fdf104e3de67',
-            'x-b3-spanid': '40c7fdf104e3de67',
-            'x-b3-sampled': '1'
-        }
+        headers = {}
         actual = self.app.get(uri, headers=headers)
         self.assertEqual(200, actual.status_code)
 
@@ -63,21 +53,11 @@ class ApplianceTest(unittest.TestCase):
         # Register expected headers with the mock. If the headers
         # don't match, the mock won't fire, an E500 will be triggered
         # and the test will fail.
-        expected_headers = {
-            'x-request-id': '34eeb41d-d267-9e49-8b84-dde403fc5b73',
-            'x-b3-traceid': '30c7fdf104e3de66',
-            'x-b3-spanid': '30c7fdf104e3de66',
-            'x-b3-sampled': '1'
-        }
+        expected_headers = { }
         m.get("http://ratings:9080/ratings/%d" % product_id, text='{}',
               request_headers=expected_headers)
 
         uri = "/api/v1/products/%d/ratings" % product_id
-        headers = {
-            'x-request-id': '34eeb41d-d267-9e49-8b84-dde403fc5b73',
-            'x-b3-traceid': '30c7fdf104e3de66',
-            'x-b3-spanid': '30c7fdf104e3de66',
-            'x-b3-sampled': '1'
-        }
+        headers = {}
         actual = self.app.get(uri, headers=headers)
         self.assertEqual(200, actual.status_code)
